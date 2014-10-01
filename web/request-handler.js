b588a385580b3696e2c1425fdc9c6ac92ccba4f1
adam-back <adamback42@gmail.com>
2014-10-01 15:54:11 -0700

    GET request reaching request-handler for / but not redirecting to the loading.html page.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,19 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
+var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === "/") {
+    if (req.method === "GET") {
+      // res.statusCode = 200;
+      res.writeHead(200, httpHelpers.headers);
+      res.end('<input');
+
+      // res.writeHead(200, {'Location': './public/loading.html'} );
+      // res.end();
+    };
+  }
+  // res.end(archive.paths.list);
 };
